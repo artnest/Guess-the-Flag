72476e0c012832ace0a1500922164006ba4d4eb1
artnest <artnest789@gmail.com>
2020-02-14 11:47:49 +0300

    Initial Commit

---
+++ Guess the Flag/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Guess the Flag
+//
+//  Created by Artyom Nesterenko on 14/2/20.
+//  Copyright © 2020 artnest. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
