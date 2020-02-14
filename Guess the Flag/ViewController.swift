eed3e524c8474cfbbd1fd7bcdd93909ecaa77fa1
artnest <artnest789@gmail.com>
2020-02-14 17:50:32 +0300

    Show player's score in the navigation bar Limit the question to the amount of 10 Show player final score Update wrong answer message with details

--- Guess the Flag/ViewController.swift
+++ Guess the Flag/ViewController.swift
@@ -29,6 +29,7 @@ class ViewController: UIViewController {
     ]
     private var correctAnswer = 0
     private var score = 0
+    private var questionsAsked = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -45,25 +46,41 @@ class ViewController: UIViewController {
     }
     
     private func askQuestion(action: UIAlertAction! = nil) {
+        if questionsAsked == 10 {
+            button1.isEnabled = false
+            button2.isEnabled = false
+            button3.isEnabled = false
+            
+            let ac = UIAlertController(title: "Game Finish", message: "Your final score is \(score)", preferredStyle: .alert)
+            ac.addAction(UIAlertAction(title: "Finish", style: .default))
+            present(ac, animated: true)
+            return
+        }
+        
         countries.shuffle()
         button1.setImage(UIImage(named: countries[0]), for: .normal)
         button2.setImage(UIImage(named: countries[1]), for: .normal)
         button3.setImage(UIImage(named: countries[2]), for: .normal)
         correctAnswer = Int.random(in: 0...2)
-        title = countries[correctAnswer].uppercased()
+        questionsAsked += 1
+        title = "\(countries[correctAnswer].uppercased()), Score: \(score)"
     }
     
     @IBAction private func buttonTapped(_ sender: UIButton) {
         let title: String
+        let message: String
         if sender.tag == correctAnswer {
-            title = "Correct"
             score += 1
+            title = "Correct"
+            message = "Your score is \(score)."
         } else {
-            title = "Wrong"
             score -= 1
+            title = "Wrong"
+            let country = countries[sender.tag].capitalized
+            message = "That's the flag of \(country)"
         }
         
-        let ac = UIAlertController(title: title, message: "Your score is \(score).", preferredStyle: .alert)
+        let ac = UIAlertController(title: title, message: message, preferredStyle: .alert)
         ac.addAction(UIAlertAction(title: "Continue", style: .default, handler: askQuestion))
         present(ac, animated: true)
     }
