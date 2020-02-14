56750bad20aaa3d262e23f50d71a9d3c6c37b164
artnest <artnest789@gmail.com>
2020-02-14 16:48:37 +0300

    Handle buttons taps

--- Guess the Flag/ViewController.swift
+++ Guess the Flag/ViewController.swift
@@ -44,12 +44,27 @@ class ViewController: UIViewController {
         askQuestion()
     }
     
-    private func askQuestion() {
+    private func askQuestion(action: UIAlertAction! = nil) {
         countries.shuffle()
         button1.setImage(UIImage(named: countries[0]), for: .normal)
         button2.setImage(UIImage(named: countries[1]), for: .normal)
         button3.setImage(UIImage(named: countries[2]), for: .normal)
         correctAnswer = Int.random(in: 0...2)
         title = countries[correctAnswer].uppercased()
+    }
+    
+    @IBAction private func buttonTapped(_ sender: UIButton) {
+        let title: String
+        if sender.tag == correctAnswer {
+            title = "Correct"
+            score += 1
+        } else {
+            title = "Wrong"
+            score -= 1
+        }
+        
+        let ac = UIAlertController(title: title, message: "Your score is \(score).", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "Continue", style: .default, handler: askQuestion))
+        present(ac, animated: true)
     }
 }
