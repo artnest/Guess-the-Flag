77b574b7af3ef23f94e08246522b6788c6e85c6b
artnest <artnest789@gmail.com>
2020-02-14 16:13:37 +0300

    Add buttons styling

--- Guess the Flag/ViewController.swift
+++ Guess the Flag/ViewController.swift
@@ -13,23 +13,39 @@ class ViewController: UIViewController {
     @IBOutlet private var button2: UIButton!
     @IBOutlet private var button3: UIButton!
     
-    private var countries = [String]()
+    private var countries = [
+        "estonia",
+        "france",
+        "germany",
+        "ireland",
+        "italy",
+        "monaco",
+        "nigeria",
+        "poland",
+        "russia",
+        "spain",
+        "uk",
+        "us"
+    ]
     private var score = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        countries.append("estonia")
-        countries.append("france")
-        countries.append("germany")
-        countries.append("ireland")
-        countries.append("italy")
-        countries.append("monaco")
-        countries.append("nigeria")
-        countries.append("poland")
-        countries.append("russia")
-        countries.append("spain")
-        countries.append("uk")
-        countries.append("us")
+        button1.layer.borderWidth = 1
+        button2.layer.borderWidth = 1
+        button3.layer.borderWidth = 1
+        
+        button1.layer.borderColor = UIColor.lightGray.cgColor
+        button2.layer.borderColor = UIColor.lightGray.cgColor
+        button3.layer.borderColor = UIColor.lightGray.cgColor
+        
+        askQuestion()
+    }
+    
+    private func askQuestion() {
+        button1.setImage(UIImage(named: countries[0]), for: .normal)
+        button2.setImage(UIImage(named: countries[1]), for: .normal)
+        button3.setImage(UIImage(named: countries[2]), for: .normal)
     }
 }
